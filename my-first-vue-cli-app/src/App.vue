44064f4767a7f6bc7cc06d683f7603042ab69e3b
Perrine Bizouarne <72566003+perrinebzn@users.noreply.github.com>
2023-03-31 23:34:17 +0200

    First commit

---
+++ my-first-vue-cli-app/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import HomeLink from './components/HomeLink.vue'
+import NavLink from './components/NavLink.vue'
+export default {
+  name: 'App',
+  components: {
+    HomeLink,
+    NavLink
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <nav>
+      <NavLink url="/" text="Accueil" />
+      <NavLink url="/about" text="A propos" />
+    </nav>
+    <p>Bienvenue sur notre page d'<HomeLink /></p>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
